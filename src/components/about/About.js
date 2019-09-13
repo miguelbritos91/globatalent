6cf6d2310fdb8a65ed59a69c972e4e9eb8edb5a0
Miguel <miguelbritos91@gmail.com>
2019-09-13 17:30:24 -0300

    Mejoras responsive y nuevo contenido

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import './About.css';
+import imggbt01 from './corpicon.28a140e6.png';
+import imggbt02 from './toni.b71e62b0.png';
 
 function About(){
     return(
-        <div className="About">
-            <div className="plx">
+        <div className="About plx">
+            <div className="">
                 <div className="container">
                     <div className="row">
-                        <div className="col-12">
-                            <h3 className="title text-center title-about text-uppercase">¿Quiénes Somos?</h3>
+                        <div className="col-md-9">
+                            <h3 className="title text-center title-about text-uppercase">¿QUÉ ES GLOBATALENT?</h3>
+                        </div>
+                        <div className="col-md-3 m-auto">
+                            <img src={ imggbt01 } className="d-block w-100" alt="..."/>
                         </div>
                     </div>
                 </div>
@@ -16,14 +21,14 @@ function About(){
             <div className="description-about">
                 <div className="container">
                     <div className="row">
-                        <div className="col-12">
-                            <p className="text-white">Globatalent es una nueva forma de inversión que te permitirá multiplicar tu inversión por 5 en el mundo del deporte.</p>
-                            <p className="text-white">Clubes y deportistas crean sus tokens que se venden en el market Globatalent durante un periodo de 3-6 meses.Los clubes/deportistas se comprometen a devolver a los compradores de los token un importe anual que puede estar ligado a % de los premios del club/deportista, % de futuros traspasos, % de derechos de tv o cualquier otro derecho que tenga el club/deportista.</p>
+                        <div className="col-auto">
+                            <p className="text-blue font-weight-bold text-justify">Globatalent es una nueva forma de inversión que te permitirá multiplicar tu inversión por 5 en el mundo del deporte.</p>
+                            <p className="text-white text-justify">Clubes y deportistas crean sus tokens que se venden en el market Globatalent durante un periodo de 3-6 meses. <br /> Los clubes/deportistas se comprometen a devolver a los compradores de los token un importe anual que puede estar ligado a % de los premios del club/deportista, % de futuros traspasos, % de derechos de tv o cualquier otro derecho que tenga el club/deportista.</p>
                         </div>
                     </div>
                 </div>
             </div>
-            <div className="plx">
+            <div className="">
                 <div className="container">
                     <div className="row">
                         <div className="col-12">
@@ -35,8 +40,11 @@ function About(){
             <div className="description-about">
                 <div className="container">
                     <div className="row">
-                        <div className="col-12">
-                            <p className="text-white">Nuestros deportistas y clubes pasan por un selectivo proceso de validación antes de entrar en nuestra plataforma, a través de este proceso nos aseguramos que el proyecto tiene rentabilidad para los compradores de los tokens. Un especialista del deporte en concreto validará el potencial futuro del deportista. Como ejemplo nuestro tenista David Vega ha sido avalado por Toni Nadal.</p>                            
+                        <div className="col-md-8">
+                            <p className="text-white text-justify">Nuestros deportistas y clubes pasan por un selectivo proceso de validación antes de entrar en nuestra plataforma, a través de este proceso nos aseguramos que el proyecto tiene rentabilidad para los compradores de los tokens. Un especialista del deporte en concreto validará el potencial futuro del deportista. Como ejemplo nuestro tenista David Vega ha sido avalado por Toni Nadal.</p>                            
+                        </div>
+                        <div className="col-md-4">
+                            <img src={ imggbt02 } className="d-block w-100" alt="..."/>
                         </div>
                     </div>
                 </div>
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,62 +1,106 @@
 import React from 'react';
 import './Header.css';
-import imgage01 from './01.jpg';
-import imgage02 from './02.jpg';
-import imgage03 from './03.jpg';
-import imgage04 from './04.jpg';
-import imgage05 from './05.jpg';
+import imgage01 from './davidvega.png';
+import imgage02 from './daniclos.png';
+import imgage03 from './miquelpons.png';
+import imgage04 from './asiaesport.png';
+import bgHeader from '../bg-header.png';
+
 
 function Header(){
     return(
         <div className="header">
-            <div id="carouselExampleIndicators" className="carousel slide" data-ride="carousel">
-                <ol className="carousel-indicators">
-                    <li data-target="#carouselExampleIndicators" data-slide-to="0" className="active"></li>
-                    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-                    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-                    <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
-                    <li data-target="#carouselExampleIndicators" data-slide-to="4"></li>
-                </ol>
-                <div className="carousel-inner">
-                    <div className="carousel-item active">
-                        <img src={ imgage01 } className="d-block w-100" alt="..."/>
-                    </div>
-                    <div className="carousel-item">
-                        <img src={ imgage02 } className="d-block w-100" alt="..."/>
-                    </div>
-                    <div className="carousel-item">
-                        <img src={ imgage03 } className="d-block w-100" alt="..."/>
-                    </div>
-                    <div className="carousel-item">
-                        <img src={ imgage04 } className="d-block w-100" alt="..."/>
-                    </div>
-                    <div className="carousel-item">
-                        <img src={ imgage05 } className="d-block w-100" alt="..."/>
-                    </div>
-                </div>
-                <a className="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span className="sr-only">Previous</span>
-                </a>
-                <a className="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span className="sr-only">Next</span>
-                </a>
-            </div>
+            <img src={bgHeader} className="d-block h-100"/>
             <div className="container-fluid title-header">
                 <div className="row">
                     <div className="col-8 mx-auto">
                         <h1 className="text-white text-center title">Apoya a deportistas y clubes a cumplir su sueño</h1>
                         <h2 className="text-white text-center pt-3 subtitle">Tu ayuda cambia vidas</h2>
                         <div className="text-center pt-1">
-                            <a className="btn btn-lg btn-outline-success more-info text-white" href="#more-info">Contribuye ahora</a>
+                            <a className="btn btn-lg btn-outline-warning more-info " href="#registers">Contribuye ahora</a>
+                        </div>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-md-8 col-lg-6 col-xl-4 mx-auto mt-3">
+                        <div className="deportistas p-2">
+                            <div id="carouselExampleIndicators" className="carousel slide" data-ride="carousel">                                
+                                <div className="carousel-inner">
+                                    <div className="carousel-item active">
+                                        <div className="row">
+                                            <div className="col-auto mx-auto">
+                                                <img src={ imgage01 } className="d-block h-100" alt="..."/>
+                                            </div>
+                                            <div className="col-auto mx-auto">
+                                                <p className="text-white"><span className="font-weight-bold">Nombre:</span> David Vega</p>
+                                                <p className="text-white"><span className="font-weight-bold">Símbolo:</span> DVT</p>
+                                                <p className="text-white"><span className="font-weight-bold">Deporte:</span> Tenis</p>
+                                                <p className="text-white"><span className="font-weight-bold">Precio:</span> 20 USD</p>
+                                                <p className="text-white"><span className="font-weight-bold">Beneficios:</span> 18% Premios ATP</p>
+                                            </div>                                        
+                                        </div>
+                                    </div>
+                                    <div className="carousel-item">
+                                        <div className="row">
+                                            <div className="col-auto mx-auto">
+                                                <img src={ imgage02 } className="d-block h-100" alt="..."/>
+                                            </div>
+                                            <div className="col-auto mx-auto">
+                                                <p className="text-white"><span class="font-weight-bold">Nombre:</span> Dani Clos</p>
+                                                <p className="text-white"><span class="font-weight-bold">Símbolo:</span> DCT</p>
+                                                <p className="text-white"><span class="font-weight-bold">Deporte:</span> Automovilismo</p>
+                                                <p className="text-white"><span class="font-weight-bold">Precio:</span> 10 USD</p>
+                                                <p className="text-white"><span class="font-weight-bold">Beneficios:</span> 10% de los premios</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="carousel-item">
+                                        <div className="row">
+                                            <div className="col-auto mx-auto">
+                                                <img src={ imgage03 } className="d-block h-100" alt="..."/>
+                                            </div>
+                                            <div className="col-auto mx-auto">
+                                                <p className="text-white"><span className="font-weight-bold">Nombre:</span> Miquel Pons</p>
+                                                <p className="text-white"><span className="font-weight-bold">Símbolo:</span> PON</p>
+                                                <p className="text-white"><span className="font-weight-bold">Deporte:</span> Motociclismo</p>
+                                                <p className="text-white"><span className="font-weight-bold">Precio:</span> 20 USD</p>
+                                                <p className="text-white"><span className="font-weight-bold">Beneficios:</span> 10% de los premios</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="carousel-item">
+                                        <div className="row">
+                                            <div className="col-auto mx-auto">
+                                                <img src={ imgage04 } className="d-block h-100" alt="..."/>
+                                            </div>
+                                            <div className="col-auto mx-auto">
+                                                <p className="text-white"><span className="font-weight-bold">Nombre:</span> Asia eSports</p>
+                                                <p className="text-white"><span className="font-weight-bold">Símbolo:</span> GTE</p>
+                                                <p className="text-white"><span className="font-weight-bold">Deporte:</span> eSports</p>
+                                                <p className="text-white"><span className="font-weight-bold">Precio:</span> 5 USD</p>
+                                                <p className="text-white"><span className="font-weight-bold">Beneficios:</span> 10% de los premios</p>
+                                            </div>
+                                        </div>
+                                    </div>                                    
+                                </div>
+                                <a className="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                                    <span className="sr-only">Previous</span>
+                                </a>
+                                <a className="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                                    <span className="sr-only">Next</span>
+                                </a>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-
         </div>
     );
 }
 
 export default Header;
+
+
+{/*  */}
